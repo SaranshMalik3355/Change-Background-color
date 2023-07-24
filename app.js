ef3edc96d9f2ae20e0fdad7fffcea0fa292e382c
Saransh <Saransh10malik@gmail.com>
2023-07-24 09:19:03 +0530

    After changes

---
+++ app.js
@@ -0,0 +1,9 @@
+// 1. Load
+// addEventListener("load", function () {
+//   this.document.body.style.backgroundColor = "red";
+// });
+
+//1. DOMContentLoaded
+addEventListener("DOMContentLoaded", function () {
+  this.document.body.style.backgroundColor = "black";
+});
