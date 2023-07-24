f3d4de9ecc45f973a518d2cbf54d53d6118b14b5
Saransh <Saransh10malik@gmail.com>
2023-07-24 10:00:53 +0530

    nothing just checking

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// 1. Load
-// addEventListener("load", function () {
-//   this.document.body.style.backgroundColor = "red";
-// });
+//1. Load
+addEventListener("load", function () {
+  this.document.body.style.backgroundColor = "red";
+});
 
 //1. DOMContentLoaded
 addEventListener("DOMContentLoaded", function () {
